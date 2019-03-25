33eca0c71bb34b4e17f47e7d99d4a762ccaf89df
wangyanwen1999 <839405933@qq.com>
2019-03-25 15:08:50 +0800

    王延文
    王延文

--- js/mitu-mini.js
+++ js/mitu-mini.js
@@ -22,3 +22,38 @@ $(function(){
 		}
 	})
 })
+	
+	$(function(){
+	var index = 1;
+	function imgPlay(){
+		$("#mitu-mini-13>img").eq(index).siblings("img").fadeOut(1000);
+		$("#mitu-mini-13>img").eq(index).fadeIn(1000);
+		index++;
+		if(index>$("#mitu-mini-13>img").length-1){
+			index=0;
+		}
+	}
+	var no = setInterval(imgPlay,3000);
+	$("#mitu-mini-13>span").click(function(){
+		index -- ;
+		if(index<0){
+			index = $("#mitu-mini-13>img").length-1;
+		}
+		$("#mitu-mini-13>img").eq(index).siblings("img").fadeOut(1000);
+		$("#mitu-mini-13>img").eq(index).fadeIn(1000);
+	})
+	$("#mitu-mini-13>span,#mitu-mini-13>em").mouseover(function(){
+		clearInterval(no);
+	})
+		$("#mitu-mini-13>span,#mitu-mini-13>em").mouseenter(function(){
+		no = setInterval(imgPlay,3000);
+	})
+	$("#mitu-mini-13>em").click(function(){
+		index ++ ;
+		if(index>$("#mitu-mini-13>img").length-1){
+			index = 0;
+		}
+		$("#mitu-mini-13>img").eq(index).siblings("img").fadeOut(1000);
+		$("#mitu-mini-13>img").eq(index).fadeIn(1000);
+	})
+})
